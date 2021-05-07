1a06a303e84dccdb2b333f8b694a46337da45c49
Rafael Zuquim <rzuquim@inoa.com.br>
2021-05-07 10:44:18 -0300

    clean-up

--- core/_/GrepOptions.cs
+++ core/_/GrepOptions.cs
@@ -4,12 +4,16 @@ namespace YAGrep {
     public class GrepOptions {
         public Encoding Encoding { get; }
         public bool SilentCancel { get; }
+        public int BufferSize { get; }
 
-        public GrepOptions(Encoding? encoding = null, bool? silentCancel = null) {
+        public GrepOptions(Encoding? encoding = null, bool? silentCancel = null, int? bufferSize = null) {
             Encoding = encoding ?? Encoding.UTF8;
             SilentCancel = silentCancel ?? true;
+            BufferSize = bufferSize ?? _2Kb;
         }
 
         public static GrepOptions Default { get; } = new();
+
+        private const int _2Kb = 2 * 1024;
     }
 }
