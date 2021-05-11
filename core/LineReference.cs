9ac78eb4f3d8a06f7ce0520cb924fe7bd922588c
Rafael Zuquim <rzuquim@inoa.com.br>
2021-05-11 18:58:28 -0300

    wip

---
+++ core/LineReference.cs
@@ -0,0 +1,18 @@
+﻿namespace YAGrep {
+    public readonly struct LineReference {
+        private readonly char[] _buffer;
+        private readonly int _startIndex;
+
+        public int Length { get; }
+
+        public LineReference(string line) {
+            _buffer = line.ToCharArray();
+            _startIndex = 0;
+            Length = _buffer.Length;
+        }
+
+        public LineReference Snapshot() => new(this.AsString());
+
+        public string AsString() => new(_buffer, _startIndex, Length);
+    }
+}
